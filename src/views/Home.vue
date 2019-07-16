5af66ed163c3c59eeb9f6dc5fc25765348879d91
gaoshengyun <18616242909@163.com>
2019-07-16 22:00:53 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span class="text">012345</span><br>
+    <span class="text">ABCDEF</span><br>
+    <span class="icon-bookmark"></span>
+    <span class="icon-clock"></span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
+<style lang="scss" scope>
+  @import '../assets/styles/global.scss';
+  .icon-bookmark{
+    font-size: 30px;
+    color: #f00;
+  }
+  .text{
+    font-family: 'Days One';
+    font-size: px2rem(30);
+    color: orange;
+  }
+</style>
+
+
